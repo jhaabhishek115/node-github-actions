7ee962a2b8c703bc8fbc17cf0575a81ad211b72e
jhaabhishek115 <jhaabhishek115@gmail.com>
2023-01-28 17:56:41 +0530

    workflow added

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+// route
+app.get("/", (req, res) => {
+  // Sending This is the home page! in the page
+  res
+    .status(200)
+    .send(`Hello World from Github actions ${process.env.PORT}`);
+});
+
+// Listening to the port 
+let PORT = process.env.PORT ? process.env.PORT : 8000;
+console.log(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running...`);
+});
+
+
